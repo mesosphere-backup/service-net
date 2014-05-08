8bc121e466901602b9b4fbf6f9836e1f0a94ed6c
Jason Dusek <jason.dusek@gmail.com>
2014-05-08 21:39:05 +0000

    Sketch for DNS records

---
+++ dsl/src/main/scala/mesosphere/servicenet/dsl/dns/record.scala
@@ -0,0 +1,47 @@
+package mesosphere.servicenet.dsl.dns
+
+import java.net.Inet6Address
+import mesosphere.servicenet.dsl.NetworkEntity
+
+/**
+  * IPv6 record
+  */
+case class AAAA(label: String, addresses: Seq[Inet6Address]) extends Record {
+  val data: Seq[String] = addresses.map(_.getHostAddress)
+}
+
+/**
+  * SRV record
+  */
+case class SRV(label: String, endpoints: Seq[SRVData] = Seq()) extends Record {
+  val data: Seq[String] = for (srv <- endpoints) yield {
+    import srv._
+    s"$priority $weight $port $target"
+  }
+  /**
+    * Clients should never cache SRV records but TTL of 0 is not to be used.
+    *
+    * http://mark.lindsey.name/2009/03/never-use-dns-ttl-of-zero-0.html
+    */
+  override val ttl: Int = 1
+}
+
+case class SRVData(
+  target: String,
+  port: Int = 0,
+  weight: Int = 1,
+  priority: Int = 1)
+
+/**
+  * See http://www.zytrax.com/books/dns/ch8/#generic
+  */
+sealed trait Record extends NetworkEntity {
+  val label: String
+  val recordType: String = getClass.getSimpleName.toUpperCase()
+  val recordClass: String = "IN" // Internet class records are the norm
+  val ttl: Int = 3600 // 1 hour
+  val data: Seq[String]
+  val records: Seq[(String, String, String, String, String)] =
+    for (d <- data) yield (label, ttl.toString, recordClass, recordType, d)
+  val name: String = label
+}
--- dsl/src/main/scala/mesosphere/servicenet/dsl/terms.scala
+++ dsl/src/main/scala/mesosphere/servicenet/dsl/terms.scala
@@ -1,39 +1,39 @@
 package mesosphere.servicenet.dsl
 
+import mesosphere.servicenet.dsl.dns.Record
+
 //////////////////////////////////////////////////////////////////////////////
 //  Diffable Service Network Descriptions  ///////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
 case class Doc(
   interfaces: Seq[Interface],
-  dns: Seq[DNS],
+  dns: Seq[Record],
   nat: Seq[NAT],
   tunnels: Seq[Tunnel])
 
 case class Diff(
   interfaces: Seq[Change[Interface]],
-  dns: Seq[Change[DNS]],
+  dns: Seq[Change[Record]],
   nat: Seq[Change[NAT]],
   tunnels: Seq[Change[Tunnel]])
 
-trait NetworkItem {
+trait NetworkEntity {
   def name(): String
 }
 
-case class Interface(name: String) extends NetworkItem
+case class Interface(name: String) extends NetworkEntity
 
-case class DNS(name: String) extends NetworkItem
+case class NAT(name: String) extends NetworkEntity
 
-case class NAT(name: String) extends NetworkItem
-
-case class Tunnel(name: String) extends NetworkItem
+case class Tunnel(name: String) extends NetworkEntity
 
 //////////////////////////////////////////////////////////////////////////////
 //  Changes  /////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////
 
-trait Change[T <: NetworkItem]
+trait Change[T <: NetworkEntity]
 
-case class Add[T <: NetworkItem](item: T) extends Change[T]
+case class Add[T <: NetworkEntity](item: T) extends Change[T]
 
-case class Remove[T <: NetworkItem](name: String) extends Change[T]
+case class Remove[T <: NetworkEntity](name: String) extends Change[T]
