e8406e71e8fc0556b27842d3bf7bcc6f5070797c
Jason Dusek <jason.dusek@gmail.com>
2014-05-18 18:59:29 +0000

    Clean up natfan and supporting functions.
    Calculates the probabilities needed by the iptables statistics modules so
    clients can just pass <ip>@<weight>.

--- patch/src/main/resources/patch.bash
+++ patch/src/main/resources/patch.bash
@@ -36,32 +36,54 @@ function tunnel {
 
 function natfan {
   local ipv6="$1" mark="$2" ; shift 2
+  [[ $# -gt 0 ]] || return 0
   local backends=( "$@" )
+  # The backends are passed as <ip>@<weight>. We parse them and put the
+  # weights and IPs in separate arrays.
+  local ips="${backends[@]%@*}" weights=( ) weight=
+  for backend in "${backends[@]}"
+  do
+    weight="${backend#*@}"
+    # For backends without an explicit weight, we assign a weight of 1.
+    [[ $weight != $backend ]] && weights+=( "$weight" ) || weights+=( 1 )
+  done
+  # We need to pass probabilities to iptables. Because the rules fallthrough,
+  # we need probabilities that stack. For example, if we have three backends
+  # with even weights, the probabilities should be 1/3, 1/2 and 1. (1/3 of all
+  # the traffic, 1/2 of two thirds of the traffic and the whole of one third
+  # of the traffic.)
+  local probabilites=( $(cascade "${weights[@]}") )
   # We "mark" the packets before rewriting the destination address in the
   # OUTPUT chain (a fairly early chain) so we can find them later in the
   # POSTROUTING chain. These marks are something internal to the Linux
-  # networking stack -- packet marks IP protocol agnostic and don't get passed
-  # on the wire.
+  # networking stack. Packet marks are IP protocol agnostic and don't get
+  # passed on the wire.
   mark "$ipv6" "$mark"
-  # The backends are passed as <ip>@<probability> but the probabilities stack
-  # so you have to give some thought to setting them. The last backend should
-  # be passed without a probability.
-  for backend in "${backends[@]}"
+  local n=0 next_to_last=$(( ${#ips[@]} - 1 ))
+  while [[ $n -lt $next_to_last ]]
   do
-    local ip="${backend%@*}" probability="${backend#*@}"
-    if [[ $probability != $backend ]] # There was an @suffix to remove...
-    then
-      # Route a percentage of the traffic that remains to this backend.
-      dnat_random "$ipv6" "$ip" "$probability"
-    else
-      # Route all remaining traffic to this backend.
-      dnat_fixed  "$ipv6" "$ip"
-    fi
-    # Ensure that traffic finds its way back to the service interface, by
-    # rewriting the source address. Do this only for packets where the
-    # destination is one of our backends and the mark matches.
-    snat "$ipv6" "$ip" "$mark"
+    # Rewrite the destination address to point to this backend.
+    dnat "$ipv6" "${ips[$n]}" "${probabilities[$n]}"
+    # Rewrite the source address, so traffic can find its way back to the
+    # client connection. Do this only for packets where the destination is one
+    # of our backends and the mark matches.
+    snat "$ipv6" "${ips[$n]}" "$mark"
+    n=$(( $n + 1 ))
   done
+  # The final backend is treated as a catchall -- no probabilities involved.
+  # For services with only one backend, we have a fast path.
+  dnat "$ipv6" "${ips[$n]}"
+  snat "$ipv6" "${ips[$n]}" "$mark"
+}
+
+function cascade {
+  while [[ $# != 0 ]]
+  do
+    # Probability needed for this weight is fraction of remaining weights. The
+    # last weight is always assigned probability 1.
+    out "scale = 9 ; $1 / ( 0 ${@/#/+ } )"
+    shift
+  done | bc
 }
 
 function mark {
@@ -69,23 +91,22 @@ function mark {
   ip6tables -t nat -A OUTPUT -d "$ipv6" -j MARK --set-mark "$mark"
 }
 
-function dnat_random {
-  # Use the statistic module to randomly pick a backend IP.
-  local ipv6="$1" backend="$2" probability="$3"
-  ip6tables -t nat -A OUTPUT -d "$ipv6" -m state --state NEW \
-            -m statistic --mode random --probability "$probability" \
-            -j DNAT --to "$backend"
-}
-
-function dnat_fixed {
+function dnat {
   local ipv6="$1" backend="$2"
-  ip6tables -t nat -A OUTPUT -d "$ipv6" -m state --state NEW \
-            -j DNAT --to "$backend"
+  if [[ ${3:+isset} ]]
+  then
+    ip6tables -t nat -A OUTPUT -d "$ipv6" -m state --state NEW \
+              -m statistic --mode random --probability "$3" \
+              -j DNAT --to "$backend"
+  else
+    ip6tables -t nat -A OUTPUT -d "$ipv6" -m state --state NEW \
+              -j DNAT --to "$backend"
+  fi
 }
 
 function snat {
   local ipv6="$1" backend="$2" mark="$3"
-  ip6tables -t nat -A POSTROUTING -d "$2" \
+  ip6tables -t nat -A POSTROUTING -d "$backend" \
             -m mark --mark "$mark" -j SNAT --to "$ipv6"
 }
 
