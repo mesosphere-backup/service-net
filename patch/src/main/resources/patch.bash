04e84d180ac350e1991c693572587c99305b591f
Jason Dusek <jason.dusek@gmail.com>
2014-05-18 22:25:21 +0000

    Fix various errors in argument passing.
    Factor out command formatting, too.

--- patch/src/main/resources/patch.bash
+++ patch/src/main/resources/patch.bash
@@ -10,37 +10,43 @@ USAGE
 }; function --help { -h ;}                 # A nice way to handle -h and --help
 
 function main {
-  : abstract
+  case "${1:-}" in
+    --dry-run) dry_run=true ; shift ;;
+  esac
+  "$@"
 }
 
 function globals {
-  export LC_ALL=en_US.UTF-8                  # A locale that works consistently
+  export LC_ALL=en_US.UTF-8
+  export LANG=en_US.UTF-8
+  dry_run=false
+  verbose=false
 }; globals
 
 function dummy {
   local name="$1" ipv6="$2"
-  ip link add "$name" type dummy
-  ip -6 addr add "$ipv6" dev "$name"
-  ip link set "$name" up
+  perform ip link add "$name" type dummy
+  perform ip -6 addr add "$ipv6" dev "$name"
+  perform ip link set "$name" up
 }
 
 function tunnel {
   local name="$1" local_ipv4="$2" remote_ipv4="$3"
   local           local_ipv6="$4" remote_ipv6="${5%/*}" remote_bits="${5#*/}"
-  ip tunnel add "$name" mode sit ttl 64 \
-                 local "$local_ipv4" remote "$remote_ipv4"
-  ip -6 addr add "$local_ipv6" dev "$name"
-  ip -6 route add "$remote_ipv6"/"$remote_bits" dev "$name" metric 1
-  ip link set "$name" up
+  perform ip tunnel add "$name" mode sit ttl 64 \
+                         local "$local_ipv4" remote "$remote_ipv4"
+  perform ip -6 addr add "$local_ipv6" dev "$name"
+  perform ip -6 route add "$remote_ipv6"/"$remote_bits" dev "$name" metric 1
+  perform ip link set "$name" up
 }
 
 function natfan {
-  local ipv6="$1" mark="$2" ; shift 2
+  local name="$1" ipv6="$2" mark="$3" ; shift 3
   [[ $# -gt 0 ]] || return 0
   local backends=( "$@" )
   # The backends are passed as <ip>@<weight>. We parse them and put the
   # weights and IPs in separate arrays.
-  local ips="${backends[@]%@*}" weights=( ) weight=
+  local ips=( "${backends[@]%@*}" ) weights=( ) weight=
   for backend in "${backends[@]}"
   do
     weight="${backend#*@}"
@@ -52,7 +58,7 @@ function natfan {
   # with even weights, the probabilities should be 1/3, 1/2 and 1. (1/3 of all
   # the traffic, 1/2 of two thirds of the traffic and the whole of one third
   # of the traffic.)
-  local probabilites=( $(cascade "${weights[@]}") )
+  local probabilities=( $(cascade "${weights[@]}") )
   # We "mark" the packets before rewriting the destination address in the
   # OUTPUT chain (a fairly early chain) so we can find them later in the
   # POSTROUTING chain. These marks are something internal to the Linux
@@ -88,26 +94,45 @@ function cascade {
 
 function mark {
   local ipv6="$1" mark="$2"
-  ip6tables -t nat -A OUTPUT -d "$ipv6" -j MARK --set-mark "$mark"
+  perform ip6tables -t nat -A OUTPUT -d "$ipv6" -j MARK --set-mark "$mark"
 }
 
 function dnat {
   local ipv6="$1" backend="$2"
   if [[ ${3:+isset} ]]
   then
-    ip6tables -t nat -A OUTPUT -d "$ipv6" -m state --state NEW \
-              -m statistic --mode random --probability "$3" \
-              -j DNAT --to "$backend"
+    perform ip6tables -t nat -A OUTPUT -d "$ipv6" -m state --state NEW \
+                      -m statistic --mode random --probability "$3" \
+                      -j DNAT --to "$backend"
   else
-    ip6tables -t nat -A OUTPUT -d "$ipv6" -m state --state NEW \
-              -j DNAT --to "$backend"
+    perform ip6tables -t nat -A OUTPUT -d "$ipv6" -m state --state NEW \
+                      -j DNAT --to "$backend"
   fi
 }
 
 function snat {
   local ipv6="$1" backend="$2" mark="$3"
-  ip6tables -t nat -A POSTROUTING -d "$backend" \
-            -m mark --mark "$mark" -j SNAT --to "$ipv6"
+  perform ip6tables -t nat -A POSTROUTING -d "$backend" \
+                    -m mark --mark "$mark" -j SNAT --to "$ipv6"
+}
+
+function perform {
+  [[ ! ( $verbose || $dry_run ) ]] || format_command _ "$@" >&2
+  [[ ! $dry_run ]] || return 0
+  if command "$@"
+  then [[ ! $verbose ]] || format_command 0 "$@" >&2
+  else
+    local code=$?
+    format_command "$code" "$@" >&2
+    return $code
+  fi
+}
+
+function format_command {
+  [[ $1 = _ ]] && printf ': call ; ' || printf ": exit/$1 ; "
+  shift
+  printf ' %q' "$@"
+  echo
 }
 
 function msg { out "$*" >&2 ;}
