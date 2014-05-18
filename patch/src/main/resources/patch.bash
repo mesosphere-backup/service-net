d2ffcab409633258ea0ba258b81705cc7df2d032
Jason Dusek <jason.dusek@gmail.com>
2014-05-18 23:38:12 +0000

    Implemented removal of network entities.

--- patch/src/main/resources/patch.bash
+++ patch/src/main/resources/patch.bash
@@ -23,6 +23,19 @@ function globals {
   verbose=false
 }; globals
 
+function remove {
+  local kind="$1" name="$2"
+  case "$kind" in
+    tunnel|dummy) perform ip link del "$name" ;;
+    natfan)       for chain in OUTPUT POSTROUTING
+                  do
+                    for rulenum in $(rules_for_name "$name" "$chain")
+                    do perform ip6tables -t nat -D "$chain" "$rulenum"
+                    done
+                  done ;;
+  esac
+}
+
 function dummy {
   local name="$1" ipv6="$2"
   perform ip link add "$name" type dummy
@@ -53,7 +66,7 @@ function natfan {
     # For backends without an explicit weight, we assign a weight of 1.
     [[ $weight != $backend ]] && weights+=( "$weight" ) || weights+=( 1 )
   done
-  # We need to pass probabilities to iptables. Because the rules fallthrough,
+  # We need to pass probabilities to iptables. Because the rules fall through,
   # we need probabilities that stack. For example, if we have three backends
   # with even weights, the probabilities should be 1/3, 1/2 and 1. (1/3 of all
   # the traffic, 1/2 of two thirds of the traffic and the whole of one third
@@ -62,25 +75,28 @@ function natfan {
   # We "mark" the packets before rewriting the destination address in the
   # OUTPUT chain (a fairly early chain) so we can find them later in the
   # POSTROUTING chain. These marks are something internal to the Linux
-  # networking stack. Packet marks are IP protocol agnostic and don't get
-  # passed on the wire.
-  mark "$ipv6" "$mark"
+  # networking stack. Packet marks are IP protocol agnostic and are not passed
+  # on the wire.
+  mark "$name" "$ipv6" "$mark"
   local n=0 next_to_last=$(( ${#ips[@]} - 1 ))
   while [[ $n -lt $next_to_last ]]
   do
     # Rewrite the destination address to point to this backend.
-    dnat "$ipv6" "${ips[$n]}" "${probabilities[$n]}"
+    dnat "$name" "$ipv6" "${ips[$n]}" "${probabilities[$n]}"
     # Rewrite the source address, so traffic can find its way back to the
     # client connection. Do this only for packets where the destination is one
     # of our backends and the mark matches.
-    snat "$ipv6" "${ips[$n]}" "$mark"
+    snat "$name" "$ipv6" "${ips[$n]}" "$mark"
     n=$(( $n + 1 ))
   done
   # The final backend is treated as a catchall -- no probabilities involved.
   # For services with only one backend, we have a fast path.
-  dnat "$ipv6" "${ips[$n]}"
-  snat "$ipv6" "${ips[$n]}" "$mark"
-}
+  dnat "$name" "$ipv6" "${ips[$n]}"
+  snat "$name" "$ipv6" "${ips[$n]}" "$mark"
+} # TODO: Create a chain for these natfans and use iptables-restore to manage
+########  the chain in aggregate. This should be more performant
+########  (iptables-restore does the rule changes all-of-a-piece) and it will
+########  be much safer in the presence of user-defined rules.
 
 function cascade {
   while [[ $# != 0 ]]
@@ -93,43 +109,57 @@ function cascade {
 }
 
 function mark {
-  local ipv6="$1" mark="$2"
-  perform ip6tables -t nat -A OUTPUT -d "$ipv6" -j MARK --set-mark "$mark"
+  local name="$1" ipv6="$2" mark="$3"
+  perform ip6tables -t nat -A OUTPUT -d "$ipv6" \
+                    -m comment --comment servicenet//"$name" \
+                    -j MARK --set-mark "$mark"
 }
 
 function dnat {
-  local ipv6="$1" backend="$2"
-  if [[ ${3:+isset} ]]
+  local name="$1" ipv6="$2" backend="$3"
+  if [[ ${4:+isset} ]]
   then
     perform ip6tables -t nat -A OUTPUT -d "$ipv6" -m state --state NEW \
-                      -m statistic --mode random --probability "$3" \
+                      -m comment --comment servicenet//"$name" \
+                      -m statistic --mode random --probability "$4" \
                       -j DNAT --to "$backend"
   else
     perform ip6tables -t nat -A OUTPUT -d "$ipv6" -m state --state NEW \
+                      -m comment --comment servicenet//"$name" \
                       -j DNAT --to "$backend"
   fi
 }
 
 function snat {
-  local ipv6="$1" backend="$2" mark="$3"
+  local name="$1" ipv6="$2" backend="$3" mark="$4"
   perform ip6tables -t nat -A POSTROUTING -d "$backend" \
+                    -m comment --comment servicenet//"$name" \
                     -m mark --mark "$mark" -j SNAT --to "$ipv6"
 }
 
+function rules_for_name {
+  local name="$1" chain="$2"
+  perform ip6tables -t nat -n --line-numbers -L "$chain" |
+  maybe fgrep "/* servicenet//$name */" | maybe egrep -o '^[0-9]+' |
+  sort -rn # We want to delete the rules in reverse numerical order because if
+           # we don't the indices change underneath us and the deletes will
+           # hit the wrong rules or simply fail.
+}
+
 function perform {
-  [[ ! ( $verbose || $dry_run ) ]] || format_command _ "$@" >&2
-  [[ ! $dry_run ]] || return 0
+  ! ( $verbose || $dry_run ) || format_command _ "$@" >&2
+  ! $dry_run || return 0
   if command "$@"
-  then [[ ! $verbose ]] || format_command 0 "$@" >&2
+  then ! $verbose || format_command 0 "$@" >&2
   else
     local code=$?
     format_command "$code" "$@" >&2
-    return $code
+    return "$code"
   fi
 }
 
 function format_command {
-  [[ $1 = _ ]] && printf ': call ; ' || printf ": exit/$1 ; "
+  [[ $1 = _ ]] && printf ':; ' || printf ": exit/$1 ; "
   shift
   printf ' %q' "$@"
   echo
--- patch/src/main/scala/mesosphere/servicenet/patch/bash/Command.scala
+++ patch/src/main/scala/mesosphere/servicenet/patch/bash/Command.scala
@@ -11,7 +11,7 @@ sealed trait Command {
 object Command {
   case class Interface(change: dsl.Change[dsl.Interface]) extends Command {
     val command = change match {
-      case dsl.Remove(name) => Seq("rm", "dummy", name)
+      case dsl.Remove(name) => Seq("remove", "dummy", name)
       case dsl.Add(item) =>
         Seq("dummy", item.name) ++ item.addrs.map(_.getHostAddress)
     }
@@ -22,7 +22,7 @@ object Command {
 
   case class NAT(change: dsl.Change[dsl.NAT]) extends Command {
     val command = change match {
-      case dsl.Remove(name) => Seq("rm", "natfan", name)
+      case dsl.Remove(name) => Seq("remove", "natfan", name)
       case dsl.Add(item) => {
         import item._
         Seq("natfan", name, subnet.getCanonicalForm) ++
@@ -35,7 +35,7 @@ object Command {
 
   case class Tunnel(change: dsl.Change[dsl.Tunnel]) extends Command {
     val command = change match {
-      case dsl.Remove(name) => Seq("rm", "tunnel", name)
+      case dsl.Remove(name) => Seq("remove", "tunnel", name)
       case dsl.Add(tun @ Tunnel6in4(name, local, remote, addr, net)) => Seq(
         "tunnel",
         name,
