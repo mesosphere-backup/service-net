b3c389d287f34b1a9d71400982e02d27795d29d6
Jason Dusek <jason.dusek@gmail.com>
2014-05-20 21:15:35 +0000

    Fix doc formatting.

--- util/src/main/scala/mesosphere/servicenet/util/Bits.scala
+++ util/src/main/scala/mesosphere/servicenet/util/Bits.scala
@@ -5,13 +5,13 @@ import scala.collection.BitSet
 
 object Bits {
   /**
-    * Big-endian `BitSet` for a `Byte`.
+    * Big-endian [[BitSet]] for a [[Byte]].
     */
   def toBitSet(b: Byte): BitSet = BitSet((0 to 7).filter(isSet(b, _)): _*)
 
   /**
-    * Convert an array of `Byte`s to a big-endian `BitSet`. The first `Byte` is
-    * treated as the most significant.
+    * Convert an array of [[Byte]]s to a big-endian [[BitSet]]. The first
+    * [[Byte]] is treated as the most significant.
     */
   def toBitSet(bytes: Seq[Byte]): BitSet = {
     val bits: Seq[Boolean] = bytes.map(b => (0 to 7).map(isSet(b, _))).flatten
@@ -23,7 +23,7 @@ object Bits {
   def toBitSet(inet: InetAddress): BitSet = toBitSet(inet.getAddress)
 
   /**
-    * Determine whether a bit is set in a `Byte`, according to a big-endian
+    * Determine whether a bit is set in a [[Byte]], according to a big-endian
     * interpretation. Bit 0 is the most significant bit.
     */
   def isSet(byte: Byte, bit: Int): Boolean = ((byte >> (7 - bit)) & 1) == 1
